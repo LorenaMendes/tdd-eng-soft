072c24196ce65074823a13e0e53be56af75c1cf5
LorenaMendes <lorenamendes.contato@gmail.com>
2020-10-05 15:54:29 -0300

    Tarefa 4

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 class Main {
-  
+
   public static void main(String[] args) {
     MoneyTest mt = new MoneyTest();
     mt.testEquality();
+    mt.testCurrency();
 
     DollarTest dt = new DollarTest();
     dt.testMultiplication();
 
     FrancTest ft = new FrancTest();
     ft.testFrancMultiplication();
-    
   }
+  
 }
--- MoneyTest.java
+++ MoneyTest.java
@@ -2,11 +2,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 class MoneyTest{
+  
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
         assertTrue(Money.franc(5).equals(Money.franc(5)));
         assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 }
